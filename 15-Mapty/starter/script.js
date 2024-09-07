1a73d4cb6877c142011ab7ed0bc74b24a5516a74
Aaron Pope <aapope@centralstatesfunds.org>
2024-09-07 08:50:22 -0500

    adding a 3rd party library - Leaflet

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -21,7 +21,20 @@ if (navigator.geolocation)
       console.log('longitude is ', longitude);
       console.log('accuracy is within x meters: ', accuracy);
       console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
+
     function () {
       alert('Could not get your position');
     }
