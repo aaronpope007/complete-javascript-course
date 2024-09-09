1aee5c43737e4747a4729acdb8986173b3837b1b
Aaron Pope <aapope@centralstatesfunds.org>
2024-09-09 13:24:53 -0500

    displaying a marker on the map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -31,12 +31,26 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log('mapEvent', mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        console.log('lat', lat);
+        console.log('lng', lng);
 
-      map.on();
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+      });
     },
 
     function () {
