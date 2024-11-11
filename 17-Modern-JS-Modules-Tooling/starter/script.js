ee5848cd7a0ff711602f8000d6e352b300b86a4b
Aaron Pope <aapope@centralstatesfunds.org>
2024-11-11 17:21:53 -0600

    Top level await  #274

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -4,11 +4,43 @@
 // console.log(price, tq);
 // console.log('importing module');
 
-// import * as ShoppingCart from './shoppingCart.js';
+import * as ShoppingCart from './shoppingCart.js';
 // ShoppingCart.addToCart('bread', 5);
-import add, { cart } from './shoppingCart.js';
+// import add, { cart } from './shoppingCart.js';
 
-add('pizza', 2);
-add('bread', 5);
-add('apples', 2);
-console.log('cart from script', cart);
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 2);
+// console.log('cart from script', cart);
+// console.log('starting');
+// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // this await is blocking function of the rest of the code, but can be useful, but many times, harmful
+
+// const data = await res.json();
+// console.log('data', data);
+// console.log('something really');
+
+const getFirstPost = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+
+  return { title: data.at(1).title, text: data.at(1).body };
+};
+const firstPost = getFirstPost();
+firstPost.then(first => console.log(first));
+
+const getLastPost = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await res.json();
+  //   console.log('data', data);
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+// console.log('lastpost', lastPost);
+// not very clean
+// lastPost.then(last => console.log(last));
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
+// top level await will block the entire module. it can be helpful, but we must use it with great care as well
